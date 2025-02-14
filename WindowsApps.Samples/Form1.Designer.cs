f996e766ba3c4057458fd90bd1ab7e39157ae987
trevoirwilliams <braniac22@hotmail.com>
2025-02-13 19:10:46 -0500

    Add label and update UI elements in Form1
    - Introduced a new label (`label1`) with custom properties.
    - Adjusted `textBox1` to be multiline and resized it.
    - Moved `button1` to a new location and changed its text to "Click Me".
    - Added an empty `Form1_Load` event handler for future use.

--- WindowsApps.Samples/Form1.Designer.cs
+++ WindowsApps.Samples/Form1.Designer.cs
@@ -30,33 +30,47 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(243, 199);
+            this.textBox1.Location = new System.Drawing.Point(118, 199);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(225, 130);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(349, 197);
+            this.button1.Location = new System.Drawing.Point(349, 306);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Click Me";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 80.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(98, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(725, 120);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "This is Form 1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1488, 759);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,6 +80,7 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
